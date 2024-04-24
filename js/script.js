25e38e804d60b33a5b727e063c9466c5dd6203bc
gabrielbaltar <201902046293@alunos.estacio.br>
2024-04-24 17:25:31 -0300

    Dark mode

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// Adicionando Dark mode no Portfólio 
+
+const htmlBody = document.querySelector(".light-dark-mode-icon");
+
+
+htmlBody.addEventListener("click", () => {
+
+    console.log("click");
+    const body = document.body;
+    const themeIcon = document.getElementById("change-icon-mode");
+    const whiteArrow = document.getElementById("white-arrow");
+    const whiteArrow2 = document.getElementById("white-arrow-two");
+    const currentTheme = document.body.getAttribute("data-theme");
+    const externalLink = document.getElementById("external-link");
+    console.log(externalLink);
+    
+    if(body.getAttribute("data-theme") === "dark") {
+        body.removeAttribute("data-theme", "light");
+        themeIcon.src = "./img/to-light-mode.svg";
+        whiteArrow.src = "./img/white-arrow.svg";
+        whiteArrow2.src = "./img/white-arrow-two.svg";
+        
+        externalLink.src = "./img/white-external-link.svg";
+    }else {
+        body.setAttribute("data-theme", "dark");
+        themeIcon.src = "./img/to-dark-mode.svg";
+        whiteArrow.src = "./img/arrow.svg";
+        whiteArrow2.src = "./img/arrow-two.svg";
+        externalLink.src = "./img/externallink.svg";
+
+    }
+
+});
+
