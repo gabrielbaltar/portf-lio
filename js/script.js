ef4e27ec6f8e7fc06c71e6aafe0931fbe212840d
gabrielbaltar <201902046293@alunos.estacio.br>
2024-04-25 17:02:29 -0300

    Atualização Dark mode 2

--- js/script.js
+++ js/script.js
@@ -10,9 +10,10 @@ htmlBody.addEventListener("click", () => {
     const themeIcon = document.getElementById("change-icon-mode");
     const whiteArrow = document.getElementById("white-arrow");
     const whiteArrow2 = document.getElementById("white-arrow-two");
+    const menuIconMobile = document.getElementById("menu-icon-mobile");
     const currentTheme = document.body.getAttribute("data-theme");
     const externalLink = document.getElementById("external-link");
-    console.log(externalLink);
+    
     
     if(body.getAttribute("data-theme") === "dark") {
         body.removeAttribute("data-theme", "light");
@@ -21,14 +22,15 @@ htmlBody.addEventListener("click", () => {
         whiteArrow2.src = "./img/white-arrow-two.svg";
         
         externalLink.src = "./img/white-external-link.svg";
+        menuIconMobile.src = "./img/menu-icon-dark-mode.svg";
     }else {
         body.setAttribute("data-theme", "dark");
         themeIcon.src = "./img/to-dark-mode.svg";
         whiteArrow.src = "./img/arrow.svg";
         whiteArrow2.src = "./img/arrow-two.svg";
         externalLink.src = "./img/externallink.svg";
+        menuIconMobile.src = "./img/menu-icon-light-mode.svg";
 
     }
 
 });
-
