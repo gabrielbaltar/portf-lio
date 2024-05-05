5f241a9cea2e56dddb9cfea2b9fc454e2d55734c
gabrielbaltar <201902046293@alunos.estacio.br>
2024-05-05 16:57:11 -0300

    Atualizações cards

--- js/script.js
+++ js/script.js
@@ -15,25 +15,26 @@ htmlBody.addEventListener("click", () => {
     const menuIconMobile2 = document.getElementById("menu-icon-mobile2");
     const currentTheme = document.body.getAttribute("data-theme");
     const externalLink = document.getElementById("external-link");
-    const githubIcon = document.querySelectorAll(".github-dark-mode-change img");
+    //const githubIcon = document.querySelectorAll(".github-dark-mode-change img");
     const externalLinkIcon = document.querySelectorAll(".external-link-change-mode img");
-    const piukaLogo = document.getElementById("piuka-logo");
-    const deloitteLogo = document.getElementById("deloitte-icon");
+    //const piukaLogo = document.getElementById("piuka-logo");
+    //const deloitteLogo = document.getElementById("deloitte-icon");
     const githubSmallIcon = document.getElementById("github-small-icon");
+
 
     if(body.getAttribute("data-theme") === "dark") {
         body.removeAttribute("data-theme", "light");
         themeIcon.src = "./img/to-light-mode.svg";
         whiteArrow.src = "./img/white-arrow.svg";
         //whiteArrow2.src = "./img/white-arrow-two.svg";
-        piukaLogo.src = "./img/white-piuka-logo.svg"
+        //piukaLogo.src = "./img/white-piuka-logo.svg"
         menuIconMobile.src = "./img/menu-icon-dark-mode.svg";
-        deloitteLogo.src = "./img/white-deloitte-logo.svg";
+        //deloitteLogo.src = "./img/white-deloitte-logo.svg";
         githubSmallIcon.src = "./img/github-small-icon.svg";
 
-        githubIcon.forEach(icon => {
+        /*githubIcon.forEach(icon => {
             icon.src = "./img/white-github-icon.svg";
-        });
+        });*/
 
         externalLinkIcon.forEach(icon => {
             icon.src = "./img/white-external-link.svg";
@@ -43,14 +44,14 @@ htmlBody.addEventListener("click", () => {
         body.setAttribute("data-theme", "dark");
         themeIcon.src = "./img/to-dark-mode.svg";
         whiteArrow.src = "./img/arrow.svg";
-        piukaLogo.src = "./img/piuka.svg"
+        //piukaLogo.src = "./img/piuka.svg"
         menuIconMobile.src = "./img/menu-icon-light-mode.svg";
-        deloitteLogo.src = "./img/deloitte.svg";
+        //deloitteLogo.src = "./img/deloitte.svg";
         githubSmallIcon.src = "./img/github.svg";
 
-        githubIcon.forEach(icon => {
+        /*githubIcon.forEach(icon => {
             icon.src = "./img/github2.svg";
-        });
+        });*/
 
         externalLinkIcon.forEach(icon => {
             icon.src = "./img/externallink.svg";
